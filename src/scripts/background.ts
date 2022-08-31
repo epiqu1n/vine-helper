e3e809e4e39f656caf2ef54a6dbd36c2c9bf187d
epiqu1n <epiqu1n@gmail.com>
2022-08-30 20:47:25 -0400

    Set up basics for retrieving current items

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,24 +1,60 @@
+import { Category } from '../types/Items';
+import MessageTypes, { MessageResponses } from '../types/MessageTypes';
 import { RuntimeMessage } from '../types/Runtime';
-
+import { sendTabMessage } from './modules/messenger';
 console.log('Running Vine Helper background script');
+
+const categories: { [cat: string]: Category } = {};
 
 /// Globals
 let activeTabId: number;
-chrome.tabs.query({ active: true }).then((tab) => activeTabId = tab[0].id as number);
+// chrome.tabs.query({ active: true }).then((tab) => activeTabId = tab[0].id as number);
 
-// Listen for messages from popup or content script
+/// Tab event listeners
+/** onMessage: Listen for messages from popup or content script */
 chrome.runtime.onMessage.addListener((message: RuntimeMessage, sender, sendResponse) => {
   console.log('Background received a message:', message);
 
   switch (message.type) {
+    case 'RECEIVE_ITEMS':
+      // TODO: Determine which items are new and display them
+      // TODO: Update item cache for category
+      break;
     default:
       console.error(`Unknown message type: ${message.type}`);
       break;
   }
 });
 
-/// Tab event listeners
-// On change tabs: Set active tab id to current tab id
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  activeTabId = activeInfo.tabId;
+/** onUpdated: Get all items for the current category and display any new ones */
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tabInfo) => {
+  const { status } = changeInfo;
+  if (!tabInfo.url?.match(/amazon\.com\/vine\/vine-items/i) || status !== 'complete') return;
+
+  // Get URL params for category
+  const { queue, pn, cn } = Object.fromEntries((new URL(tabInfo.url)).searchParams);
+  const category = `?queue=${queue}&pn=${pn || ''}&cn=${cn || ''}`;
+  
+  // Request items for category from contents
+  console.debug('tabId:', tabId);
+  await timeout(2500); // DEBUG
+  sendTabMessage(tabId, 'GET_ITEMS_BY_ID', {
+    url: `${category}`
+  }, (response) => {
+    // TODO: Determine which items are new and display them
+    // TODO: Update item cache for category
+
+    // TODO: Response keeps coming back undefined?
+    console.debug('response:', response);
+  });
+
+  console.debug('category:', category);
 });
+
+/// Aux functions
+
+function timeout(ms: number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(undefined), ms);
+  });
+}
--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,13 +1,46 @@
-// This script has access to the DOM
-
+import { ItemSet } from '../types/Items';
+import mt from '../types/MessageTypes';
 import { RuntimeMessage } from '../types/Runtime';
+import { sendBgMessage } from './modules/messenger';
 console.log('Running Vine Helper content script');
 
-// Listen for messages from background script
-chrome.runtime.onMessage.addListener((message: RuntimeMessage, sender, sendResponse) => {
-  console.log('Content script received a message:', message);
+/// Globals
+const domParser = new DOMParser();
+
+/** onMessage: Listen for messages from popup or content script */
+chrome.runtime.onMessage.addListener(async (message: RuntimeMessage, sender, sendResponse) => {
+  console.debug('Content script received a message:', message);
 
   switch (message.type) {
+    case 'GET_ITEMS_BY_ID':
+      const { url } = message.payload as mt[typeof message.type];
+      const items: ItemSet = {};
+
+      // Determine number of pages
+      const maxPage = parseInt((document.querySelector('ul.a-pagination > li.a-normal:nth-last-of-type(2) > a') as HTMLLinkElement)?.innerText) || 1;
+
+      // Iterate through number of pages
+      for (let i = 1; i <= maxPage; i++) {
+        const pag_url = `${url}&page=${i}`;
+        const raw_page = await fetch(pag_url).then(res => res.text()) as string;
+        const page = domParser.parseFromString(raw_page, 'text/html');
+
+        // Find items and store their data
+        const itemElems = page.querySelectorAll('.vvp-item-tile') as unknown as HTMLElement[];
+        for (const itemEl of itemElems) {
+          const href = (itemEl.querySelector('.vvp-item-product-title-container .a-link-normal') as HTMLLinkElement).href;
+          items[href] = {
+            recId: itemEl.dataset.recommendationId as string,
+            imageUrl: itemEl.dataset.imgUrl as string,
+            inputBtn: (itemEl.querySelector('.vvp-details-btn') as HTMLElement).outerHTML,
+            title: (itemEl.querySelector('.a-link-normal .a-truncate-full') as HTMLSpanElement).innerText
+          };
+        }
+      }
+      console.log(items);
+      
+      sendResponse({ type: 'RECEIVE_ITEMS', payload: items });
+      break;
     default:
       console.error(`Unknown message type: ${message.type}`);
       break;
