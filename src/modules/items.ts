2d4ec0a0a1ba71d2dbd2c64c184dca1ba4760292
epiqu1n <epiqu1n@gmail.com>
2022-09-02 23:20:10 -0400

    Added get and set methods for last stored items

--- src/modules/items.ts
+++ src/modules/items.ts
@@ -2,7 +2,7 @@ import { ItemSet } from '../types/Items';
 const domParser = new DOMParser();
 
 /** Gets all of the items for the given category url */
-export async function getItemsForCategory(url: string) {
+export async function getCurrentItems(catUrl: string) {
   const items: ItemSet = {};
 
   // Determine number of pages
@@ -10,15 +10,20 @@ export async function getItemsForCategory(url: string) {
 
   // Iterate through number of pages
   for (let i = 1; i <= maxPage; i++) {
-    const pag_url = `${url}&page=${i}`;
+    const pag_url = `${catUrl}&page=${i}`;
     const raw_page = await fetch(pag_url).then(res => res.text()) as string;
     const page = domParser.parseFromString(raw_page, 'text/html');
 
     // Find items and store their data
     const itemElems = page.querySelectorAll('.vvp-item-tile') as unknown as HTMLElement[];
     for (const itemEl of itemElems) {
-      const href = (itemEl.querySelector('.vvp-item-product-title-container .a-link-normal') as HTMLLinkElement).href;
-      items[href] = {
+      const sku = (itemEl.querySelector('.vvp-item-product-title-container .a-link-normal') as HTMLLinkElement)?.href.split('amazon.com/dp/')[1];
+      if (!sku) {
+        console.error('Could not find href for item:', itemEl);
+        continue;
+      }
+
+      items[sku] = {
         recId: itemEl.dataset.recommendationId as string,
         imageUrl: itemEl.dataset.imgUrl as string,
         inputBtn: (itemEl.querySelector('.vvp-details-btn') as HTMLElement).outerHTML,
@@ -28,4 +33,17 @@ export async function getItemsForCategory(url: string) {
   }
 
   return items;
+}
+
+/** Gets the last stored items for a given category url */
+export async function getLastItems(catUrl: string): Promise<ItemSet> {
+  const catKey = `cached-items-${catUrl}`;
+  const items = (await chrome.storage.local.get(catKey))[catKey] as ItemSet;
+  return items;
+}
+
+/** Updates the last stored items for a given category url */
+export async function setLastItems(catUrl: string, items: ItemSet): Promise<void> {
+  const catKey = `cached-items-${catUrl}`;
+  await chrome.storage.local.set({ [catKey]: items});
 }
