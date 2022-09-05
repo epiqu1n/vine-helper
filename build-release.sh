892b203820a2f444452a7806d930f76c38fb53d5
epiqu1n <epiqu1n@gmail.com>
2022-09-05 14:17:50 -0400

    Created release build script

---
+++ build-release.sh
@@ -0,0 +1,27 @@
+# Get args
+while getopts "v:" arg
+do
+  case "${arg}" in
+    v) version=$OPTARG;;
+  esac
+done
+
+# Append version to filename if provided
+zipname="VineHelper"
+if [ -n "${version}" ]
+then
+  zipname="${zipname}_v${version}"
+fi
+
+echo "Building dist for release..."
+
+NODE_ENV=production npx webpack
+
+rm -rf "VineHelper"
+mkdir -p ".tmp"
+cp -r "dist" ".tmp/VineHelper"
+
+mkdir -p "builds"
+zip -r "builds/${zipname}.zip" ".tmp/VineHelper"
+
+rm -rf ".tmp/VineHelper"
